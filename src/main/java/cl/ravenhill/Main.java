fce5b325a19f209891d14cd1e94d54b21253e26b
R8V <ignacio.slater@ug.uchile.cl>
2022-06-23 14:43:40 -0400

    Init

---
+++ src/main/java/cl/ravenhill/Main.java
@@ -0,0 +1,13 @@
+package cl.ravenhill;/*
+ * "${PROJECT_NAME}" (c) by R8V.
+ * "${PROJECT_NAME}" is licensed under a
+ * Creative Commons Attribution 4.0 International License.
+ * You should have received a copy of the license along with this
+ * work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
+ */
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
